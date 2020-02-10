77c706eecd168730d94af28905985e7d464d78f0
Michael Le <mnle529@gmail.com>
2020-02-09 16:51:10 -0800

    completed

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ I WANT to be able to delete notes I've written before
 <br>
 SO THAT I can organize my thoughts and keep track of tasks I need to complete
 
+## Functionality
+
+Users and can create a new note title and text, then they can save it by clicking the save icon to store the note. You can view the note by clicking on the note title on the left. You can also delete the note byt clicking the delete icon. 
+
--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const fs = require("fs");
 
 // Starts up the express server
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the express app to handle the data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname, + "/public"));
 
-// routes
+// Routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
@@ -21,31 +21,54 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-// displays notes
+app.get("/assets/js/index.js", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/assets/js/index.js"));
+});
+
+app.get("/assets/css/styles.css", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/assets/css/styles.css"));
+});
+
+// API Routes
 app.get("/api/notes", (req, res) => {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            throw err;
-        }
-        console.log(data);
-        res.json(JSON.parse(data));
-    });
+    res.sendFile(path.join(__dirname, "./db/db.json"));
 });
 
 
-app.get("/api/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
+    let newNote = req.body;
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            throw err;
+        if (err) throw err;
+
+        let noteDB = JSON.parse(data);
+
+        noteDB.push(newNote);
+
+        let newId = 1
+        for (let i = 0; i < noteDB.length; i++) {
+            noteDB[i].id = newId++;
         }
-        let temp = JSON.parse(data);
-        console.log(temp);
-        temp.push(req.body)
-        fs.writeFile('./db/db.json', JSON.stringify(temp), (err) => {
-            if (err) {
-                throw err;
-            }
-            res.json('The file has been saved')
+
+        fs.writeFile("./db/db.json", JSON.stringify(noteDB), function (err) {
+            if (err) throw err;
+
+            return res.status(200).send("New note added");
+        })
+    });
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) throw err;
+        let noteDB = JSON.parse(data);
+        var uniqueId = parseInt(req.params.id);
+
+        const updateDB = noteDB.filter(obj => obj.id !== uniqueId);
+
+        fs.writeFile("./db/db.json", JSON.stringify(updateDB), function (err) {
+            if (err) throw err;
+
+            return res.status(200).send("Note Deleted");
         })
     });
 });
